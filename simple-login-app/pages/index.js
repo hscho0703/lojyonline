9819a590a90c18981ab3f3ea1aba893439821d38
hscho0703 <hscho0703@gmail.com>
2025-04-12 10:43:45 -0500

    Add index page

--- simple-login-app/pages/index.js
+++ simple-login-app/pages/index.js
@@ -1,18 +1,19 @@
 // pages/index.js
 
 export default function Home() {
-  return (
-    <div style={{ 
-      backgroundColor: 'black',
-      color: 'white',
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column'
-    }}>
-      <h1>L.O.J.Y</h1>
-      <p>Welcome to your site 🎉</p>
-    </div>
-  );
-}
+    return (
+      <div style={{
+        backgroundColor: 'black',
+        color: 'white',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}>
+        <h1>L.O.J.Y</h1>
+        <p>Welcome to your site 🎉</p>
+      </div>
+    );
+  }
+  
